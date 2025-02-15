a425104d4ea8fecb9fcb944be2cb83c54e1f8de5
kokk <kokk7310@gmail.com>
2025-02-15 17:45:07 +0900

    #6 - Springdoc-openapi 추가

--- item-service/src/main/java/org/msa/item/controller/ItemController.java
+++ item-service/src/main/java/org/msa/item/controller/ItemController.java
@@ -1,23 +1,24 @@
 package org.msa.item.controller;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.msa.item.dto.ItemDTO;
 import org.msa.item.dto.ResponseDTO;
-import org.msa.item.dto.constant.ItemType;
-import org.msa.item.exception.ApiException;
 import org.msa.item.service.ItemService;
 import org.msa.item.valid.ItemTypeValid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+@OpenAPIDefinition(info = @Info(title = "물품 처리요청 API", description = "물품 처리요청 API", version = "v1"))
 @RestController
 @RequestMapping(value="v1/item")
 @Slf4j
@@ -27,6 +28,11 @@ public class ItemController {
 
   private final ItemService itemService;
 
+  @Operation(summary = "물품등록 요청", description = "물품 등록을 진행할 수 있다.", tags = { "addItem" })
+  @ApiResponses({ // 문서에서 응답값에 대한 정의를 추가함
+          @ApiResponse(responseCode = "200", description = "SUCCESS"),
+          @ApiResponse(responseCode = "501", description = "API EXCEPTION")
+  })
   @RequestMapping(value="/add/{itemType}", method = RequestMethod.POST)
   public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO,@ItemTypeValid @PathVariable String itemType) {
     ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
