76e2c93a824bafcb6f329232b7d4857c4ead60b6
kokk <kokk7310@gmail.com>
2025-03-05 23:50:24 +0900

    #11 - Kafka 적용
    물품 등록시 물품 히스토리에 데이터 전달을 kafka를 활용함.

--- item-service/src/main/java/org/msa/item/service/ItemService.java
+++ item-service/src/main/java/org/msa/item/service/ItemService.java
@@ -1,11 +1,15 @@
 package org.msa.item.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.msa.item.domain.Item;
 import org.msa.item.dto.ItemDTO;
 import org.msa.item.feign.HistoryFeignClient;
 import org.msa.item.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,7 +26,11 @@ public class ItemService {
 
   private final HistoryFeignClient historyFeignClient;
   private final RestTemplate restTemplate;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  ObjectMapper objectMapper = new ObjectMapper();
 
+  @Value(value="${topic.name}")
+  private String topicName;
   public void insertItem(ItemDTO itemDTO, String accountId) {
     SimpleDateFormat form = new SimpleDateFormat("yyyyMMddHHmmss");
     String date = form.format(new Date());
@@ -45,6 +53,11 @@ public class ItemService {
     historyMap.put("itemId", itemDTO.getId());
     //log.info("feign result = {}", historyFeignClient.saveHistory(historyMap));
     // String.class 파라미터는 response 응답 타입을 설정한것
-    log.info("restTemplate result = {}", restTemplate.postForObject("http://HISTORY-SERVICE/v1/history/save", historyMap, String.class  ));
+    //log.info("restTemplate result = {}", restTemplate.postForObject("http://HISTORY-SERVICE/v1/history/save", historyMap, String.class  ));
+    try{
+      this.kafkaTemplate.send(topicName, objectMapper.writeValueAsString(itemDTO));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
